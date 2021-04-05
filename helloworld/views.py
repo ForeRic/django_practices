1ac18abf45f542edda0af860c712c622a95b6fef
Jiyoon-Jeong <jeongjiyoon93@gmail.com>
2021-04-05 11:21:24 +0900

    django_sing up practices

--- helloworld/views.py
+++ helloworld/views.py
@@ -2,11 +2,27 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def hello1(request):
-    return HttpResponse('<h1>Hello World1</h1>', content_type='text/html; charset = utf-8')
+def main(request):
+    return render(request, 'helloworld/main.html')
 
-def hello2(request):
-    return render(request, 'helloworld/hello2.html')
+def hello1(request):
+    name = request.GET['name']
+    return HttpResponse(f'<h1>Hello {name} </h1><a href="/"> 메인으로가기', content_type='text/html; charset = utf-8')
+
+
 
 def tags(request):
     return render(request, 'helloworld/tags.html')
+
+def form(request):
+    return render(request, 'helloworld/form.html')
+
+def join(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    gender = request.POST['gender']
+    hobbies = request.POST.getlist("hobbies")
+    description = request.POST["desc"]
+
+    print(email, password, gender, hobbies, description)
+    return HttpResponse('Congrats! You are welcome on board!', content_type='text/plain; charset = utf-8')
