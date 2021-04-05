d14b97247974e9713169754ed6eefddd0fa0dec6
Jiyoon-Jeong <jeongjiyoon93@gmail.com>
2021-04-05 17:59:35 +0900

    django_practices

--- django_practices/urls.py
+++ django_practices/urls.py
@@ -17,13 +17,24 @@ from django.contrib import admin
 from django.urls import path
 
 import helloworld.views as helloworldviews
+import emaillist01.views as emaillist01views
+import guestbook01.views as guestbook01views
 
 urlpatterns = [
     path('', helloworldviews.main),
     path('join', helloworldviews.join),
     path('form/', helloworldviews.form),
     path('hello1/', helloworldviews.hello1),
-
     path('tags/', helloworldviews.tags),
+
+    path('emaillist01/', emaillist01views.index),
+    path('emaillist01/form', emaillist01views.form),
+    path('emaillist01/add', emaillist01views.add),
+
+    path('guestbook01/', guestbook01views.index),
+    path('guestbook01/deleteform', guestbook01views.deleteform),
+    path('guestbook01/add', guestbook01views.add),
+    path('guestbook01/delete', guestbook01views.delete),
+
     path('admin/', admin.site.urls),
 ]
